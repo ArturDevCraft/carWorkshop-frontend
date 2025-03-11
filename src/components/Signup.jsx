6f2c01f39d2945bcb79018c862af5f647722c23b
Artur <artur.czerwiak@gmail.com>
2025-03-11 12:53:48 +0100

    Add styles and new field for signup component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,7 +2,12 @@ import classes from './Signup.module.scss';
 import { useActionState } from 'react';
 import { useDispatch } from 'react-redux';
 import { sendAuthData } from '../store/auth-actions';
-import { isEmail, isNotEmpty, hasMinLength } from '../util/validation.js';
+import {
+	isEmail,
+	isNotEmpty,
+	hasMinLength,
+	isEqualToOtherValue,
+} from '../util/validation.js';
 import Input from './Input.jsx';
 import SignupToggleButton from './SignupToggleButton.jsx';
 
@@ -13,23 +18,34 @@ export default function Signup() {
 	const dispatch = useDispatch();
 
 	async function signupAction(prevFormState, formData) {
-		let errors = { email: [], password: [] };
+		let errors = { email: [], password: [], passwordConfirm: [], name: [] };
 
-		const email = formData.get('email');
-		const password = formData.get('password');
+		const userData = {
+			email: formData.get('email'),
+			password: formData.get('password'),
+			passwordConfirm: formData.get('passwordConfirm'),
+			name: formData.get('name'),
+		};
 
-		if (!isEmail(email)) {
+		if (!isEmail(userData.email)) {
 			errors.email.push('Invalid email address.');
 		}
 
-		if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
+		if (!isNotEmpty(userData.password) || !hasMinLength(userData.password, 6)) {
 			errors.password.push(
 				'You must provide a password with at least six characters.'
 			);
 		}
+		if (!isNotEmpty(userData.name) || !hasMinLength(userData.name, 2)) {
+			errors.name.push('You must provide a name.');
+		}
+
+		if (!isEqualToOtherValue(userData.password, userData.passwordConfirm)) {
+			errors.passwordConfirm.push('Passwords must be the same.');
+		}
 
 		if (errors.email.length > 0 || errors.password.length > 0) {
-			return { errors, enteredValues: { email, password } };
+			return { errors, enteredValues: userData };
 		}
 
 		try {
@@ -38,7 +54,7 @@ export default function Signup() {
 		} catch (err) {
 			errors.password.push('Invalid email or password.');
 			errors.email.push('Invalid email or password.');
-			return { errors, enteredValues: { email, password } };
+			return { errors, enteredValues: userData };
 		}
 	}
 	return (
@@ -60,9 +76,28 @@ export default function Signup() {
 					defaultValue={formState.enteredValues?.password}
 					errors={formState.errors?.password}
 				/>
+				<Input
+					name="passwordConfirm"
+					type="password"
+					placeholder="Confirm password"
+					defaultValue={formState.enteredValues?.passwordConfirm}
+					errors={formState.errors?.passwordConfirm}
+				/>
+				<Input
+					name="name"
+					type="text"
+					placeholder="Name"
+					defaultValue={formState.enteredValues?.name}
+					errors={formState.errors?.name}
+				/>
+
+				<select name="role" id="role">
+					<option value="customer">Customer</option>
+					<option value="workshop">Workshop administrator</option>
+				</select>
 
 				<button className={classes.button} type="submit">
-					LOGIN
+					SIGNUP
 				</button>
 			</form>
 			<p>
